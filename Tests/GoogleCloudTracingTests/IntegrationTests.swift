780d531682b072d05b5fa755dd177ff250bdcb40
Gustaf Rosenblad <gustaf@ccode.se>
2024-10-29 21:00:46 +0100

    chore: Added more test details to integration test

--- Tests/GoogleCloudTracingTests/IntegrationTests.swift
+++ Tests/GoogleCloudTracingTests/IntegrationTests.swift
@@ -13,27 +13,33 @@ import Foundation
     func createSpan() async throws {
         InstrumentationSystem.bootstrap(tracer)
 
-        do {
-            try await withSpan("root-span") { span in
-                span.attributes["test-attribute"] = "test-value"
+        var context = ServiceContext.current ?? .topLevel
+        context.serviceName = "test-api"
+        context.serviceVersion = "1.0.1"
 
-                try await withThrowingTaskGroup(of: Void.self) { group in
-                    for number in 1...3 {
-                        group.addTask {
-                            try await withSpan("child-span-\(number)") { span in
-                                try await Task.sleep(for: .milliseconds(50))
-                                if number == 2 {
-                                    throw SomeError()
+        try await ServiceContext.withValue(context) {
+            do {
+                try await withSpan("root-span") { span in
+                    span.attributes["test-attribute"] = "test-value"
+
+                    try await withThrowingTaskGroup(of: Void.self) { group in
+                        for number in 1...3 {
+                            group.addTask {
+                                try await withSpan("child-span-\(number)") { span in
+                                    try await Task.sleep(for: .milliseconds(50))
+                                    if number == 2 {
+                                        throw SomeError()
+                                    }
                                 }
                             }
                         }
+                        try await group.waitForAll()
                     }
-                    try await group.waitForAll()
                 }
-            }
-        } catch {
-            if !(error is SomeError) {
-                throw error
+            } catch {
+                if !(error is SomeError) {
+                    throw error
+                }
             }
         }
 
