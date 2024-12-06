4255d042805688d0713651870ed7e5f88a656767
Gustaf Rosenblad <gustaf@ccode.se>
2024-12-06 09:46:23 -0800

    chore: Bumped grpc-swift-nio-transport

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.1.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
         .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0-beta.1"),
-        .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0-beta.1"),
+        .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", revision: "cad03dbd3020b7d5311becd7bc54113172337dbf"),
         .package(url: "https://github.com/rosecoder/google-cloud-auth-swift.git", from: "1.0.0"),
         .package(url: "https://github.com/rosecoder/retryable-task.git", from: "1.1.2"),
         .package(url: "https://github.com/rosecoder/google-cloud-service-context.git", branch: "main"),
